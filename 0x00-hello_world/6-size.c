be01d8db97c1b620e49c3136748c26516e9fe157
joacasallas <joacasallas@gmail.com>
2021-10-04 17:31:48 -0500

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	char char_size= "1 byte(s)";
-	char int_size= "4 byte(s)";
-	char long_int_size= "8 byte(s)";
-	char long_long_size= "8 byte(s)";
-	char float_size= "4 byte(s)";
+	char char_size[]= "1 byte(s)";
+	char int_size[]= "4 byte(s)";
+	char long_int_size[]= "8 byte(s)";
+	char long_long_size[]= "8 byte(s)";
+	char float_size[]= "4 byte(s)";
 
-	printf("Size of a char: 1 byte(s)\n", char_size);
-	printf("Size of an int: 4 byte(s)\n", int_size);
-	printf("Size of a long int: 4 byte(s)\n", long_int_size);
-	printf("Size of a long long int: 8 byte(s)\n", long_long_size);
-	printf("Size of a float: 4 byte(s)\n", float_size);
+	printf("Size of a char: %s\n", char_size);
+	printf("Size of an int: %s\n", int_size);
+	printf("Size of a long int: %s\n", long_int_size);
+	printf("Size of a long long int: %s\n", long_long_size);
+	printf("Size of a float: %s\n", float_size);
 	return (0);
 }
