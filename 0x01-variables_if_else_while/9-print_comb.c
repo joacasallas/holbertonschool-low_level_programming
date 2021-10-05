1a1d1a5852f8e8414bff96d4dc3ee6f1029c500d
joacasallas <joacasallas@gmail.com>
2021-10-05 11:56:45 -0500

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,16 @@
 
 int main(void)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar('0' + i);
-		putchar(',');
-		putchar(' ');
+		putchar ('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
