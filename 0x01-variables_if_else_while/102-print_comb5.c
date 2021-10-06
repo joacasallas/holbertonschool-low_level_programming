7c6eed27203b6d7dce9523d360d52fb8893cdf91
joacasallas <joacasallas@gmail.com>
2021-10-06 09:37:34 -0500

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (k == i && l == j)
+					{
+					j++;
+					}
+					else if (k == 9 && l == 9)
+					{
+						putchar ('\n');
+					}
+					putchar ('0' + i);
+					putchar ('0' + j);
+					putchar(' ');
+					putchar ('0' + k);
+					putchar ('0' + l);
+					putchar (',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
