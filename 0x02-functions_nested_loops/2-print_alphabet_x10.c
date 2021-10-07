92f089caa689e2b1d09d398f68a21c73aa598def
joacasallas <joacasallas@gmail.com>
2021-10-07 13:56:32 -0500

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
