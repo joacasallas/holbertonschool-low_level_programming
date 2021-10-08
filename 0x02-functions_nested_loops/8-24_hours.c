25d390a9741d225960bbebf3400a74acb1f842bb
joacasallas <joacasallas@gmail.com>
2021-10-08 12:28:51 -0500

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,43 +8,30 @@
 
 void jack_bauer(void)
 {
-	int h1,h2,m1,m2;
+	int h1, h2, hmax, m1, m2;
 
 	for (h1 = 0; h1 <= 2; h1++)
 	{
 		if (h1 == 2)
 		{
-			for(h2 = 0; h2 <= 3; h2++)
-			{
-				for (m1 = 0; m1 <= 5; m1++)
-				{
-					for(m2 = 0; m2 <= 9; m2++)
-					{
-						_putchar('0' + h1);
-						_putchar('0' + h2);
-						_putchar(':');
-						_putchar('0' + m1);
-						_putchar('0' + m2);
-						_putchar('\n');
-					}
-				}
-			}
+			hmax = 3;
 		}
 		else
 		{
-			for(h2 = 0; h2 <= 9; h2++)
+			hmax = 9;
+		}
+		for (h2 = 0; h2 <= hmax; h2++)
+		{
+			for (m1 = 0; m1 <= 5; m1++)
 			{
-				for (m1 = 0; m1 <= 5; m1++)
+				for (m2 = 0; m2 <= 9; m2++)
 				{
-					for(m2 = 0; m2 <= 9; m2++)
-					{
-						_putchar('0' + h1);
-						_putchar('0' + h2);
-						_putchar(':');
-						_putchar('0' + m1);
-						_putchar('0' + m2);
-						_putchar('\n');
-					}
+					_putchar('0' + h1);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+					_putchar('\n');
 				}
 			}
 		}
