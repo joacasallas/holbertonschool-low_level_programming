8ab193b0befe176585f23d82ad146356bc13a51c
joacasallas <joacasallas@gmail.com>
2021-10-09 01:10:24 -0500

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,49 +11,45 @@ void print_times_table(int n)
 	int i, j;
 	int k = 0;
 
-	if (n > 15 || n < 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
 		{
-			k = i * j;
-			if (k > 9 && k < 100)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(' ');
-				_putchar('0' + k / 10);
-				_putchar('0' + k % 10);
-			}
-			else if (k > 99)
-			{
-				_putchar('0' + k / 100);
-				_putchar('0' + (k / 10) % 10);
-				_putchar('0' + k % 10);
-			}
-			else if (k == 0 && j == 0)
-			{
-				_putchar('0' + k);
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + k);
-			}
-			if (j == n)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+				k = i * j;
+				if (k > 9 && k < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + k / 10);
+					_putchar('0' + k % 10);
+				}
+				else if (k > 99)
+				{
+					_putchar('0' + k / 100);
+					_putchar('0' + (k / 10) % 10);
+					_putchar('0' + k % 10);
+				}
+				else if (k == 0 && j == 0)
+				{
+					_putchar('0' + k);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + k);
+				}
+				if (j == n)
+				{
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
-	}
 	}
 }
