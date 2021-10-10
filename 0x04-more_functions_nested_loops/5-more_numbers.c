cd83db543f2146111095ab0708cc77a19cc0035a
joacasallas <joacasallas@gmail.com>
2021-10-10 11:42:41 -0500

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,21 +8,23 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k, l;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j < 10)
 			{
-				_putchar('0' + j);
+				k = j;
 			}
 			else
 			{
-				_putchar('0' + j / 10);
-				_putchar('0' + j % 10);
+				k = j % 10;
+				l = j / 10;
+				_putchar('0' + l);
 			}
+			_putchar('0' + k);
 		}
 		_putchar('\n');
 	}
