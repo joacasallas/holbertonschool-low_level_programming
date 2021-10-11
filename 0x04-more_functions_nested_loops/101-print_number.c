c73b5ec5d7608d17d7c5244b6abe6aa3c1ac4499
joacasallas <joacasallas@gmail.com>
2021-10-10 21:49:57 -0500

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number -  prints an integer.
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	
+	if ((n >= 0) && (n <= 9))
+	{
+		_putchar('0' + n);
+	}
+	else if ((n > 9) && (n <= 99))
+	{
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+	else if ((n > 99) && (n <= 999))
+	{
+		_putchar('0' + (n / 100));
+		_putchar('0' + (n / 100 % 10));
+		_putchar('0' + (n % 100));
+	}
+	else if ((n > 999) && (n <= 9999))
+	{
+		_putchar('0' + (n / 1000));
+		_putchar('0' + (n / 100 % 10));
+		_putchar('0' + (n / 10 % 10));
+		_putchar('0' + (n % 10));
+	}
+}
