65c3a6c5a0f768d3667a40a15348c59b1ff6fb14
joacasallas <joacasallas@gmail.com>
2021-10-12 11:00:28 -0500

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,18 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = _strlen(s); i >= 0; i--)
+	for (i = _strlen(s) - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to check length
+ * Return: lenght of a string.
+ */
 
 int _strlen(char *s)
 {
