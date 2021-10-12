ea4327bb01b7ca6915e27738d5963da78ce2f495
joacasallas <joacasallas@gmail.com>
2021-10-12 14:31:56 -0500

    update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array
+ * @n: number of elements of the array to be printed
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	
+}
