b6c5b0dd2c6e63985690a8c2cab708ca3df26ddd
joacasallas <joacasallas@gmail.com>
2021-10-12 12:18:46 -0500

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string pointer
+ * Return: Nothing
+ */
+
+int _strlen(char *str);
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = (_strlen(str) / 2); i <= (_strlen(str)); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @str: string to check length
+ * Return: lenght of a string.
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
