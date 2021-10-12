da9cfd2cd196b106ecb1bf058d966f68a1a162ff
joacasallas <joacasallas@gmail.com>
2021-10-12 10:53:05 -0500

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,36 @@
  * Return: Nothing.
  */
 
+int _strlen(char *s);
+
 void rev_string(char *s)
 {
-	int i, last, j, temp;
+	char aux;
+	char *str1 = s;
+	char *str2 = s + _strlen(s) - 1;
+
+	while (str1 < str2)
+	{
+		aux = *str1;
+		*str1 = *str2;
+		*str2 = aux;
+		++str1;
+		--str2;
+	}
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to check length
+ * Return: lenght of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		last = i;
 	}
-	last = i;
-	i = 0;
-	j = last - 1;
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
+	return (i);
 }
