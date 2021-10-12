0bd2d002f462a6fd10f72c23ee828a7a8d514134
joacasallas <joacasallas@gmail.com>
2021-10-12 11:42:39 -0500

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,33 @@
  * Return: Always 0.
  */
 
+int _strlen(char *s);
+
 void puts2(char *str)
 {
 	int i;
+	int j = _strlen(str);
 
-	for (i = 0; str[i] != '\0'; i += 2)
-	{
-		_putchar(str[i]);
-	}
+	for (i = 0; i <= j; i += 2)
+		{
+			_putchar(str[i]);
+		}
 	_putchar('\n');
+}
+
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to check length
+ * Return: lenght of a string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
