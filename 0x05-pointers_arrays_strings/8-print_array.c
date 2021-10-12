067d0666edae457512b34f6893c0dbba836f9a5b
joacasallas <joacasallas@gmail.com>
2021-10-12 16:32:12 -0500

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-int _strlen(char *src);
-
-
 /**
  * print_array - prints n elements of an array of integers
  * @a: array
@@ -17,30 +14,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (a[i] == (a[n - 1]))
+		printf("%d", a[i]);
+		if (i < n - 1)
 		{
-			printf("%d", a[i]);
-		}
-		else if (a[i] < a[n])
-		{
-			printf("%d, ", a[i]);
+			printf(", ");
 		}
 	}
 	printf("\n");
-}
-
-/**
- * _strlen - returns the length of a string
- * @src: string to check length
- * Return: lenght of a string.
- */
-
-int _strlen(char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-	}
-	return (i);
 }
