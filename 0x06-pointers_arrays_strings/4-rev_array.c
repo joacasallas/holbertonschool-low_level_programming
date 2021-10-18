eeeecb3b17570453aaa05149c91e92aeb6825491
joacasallas <joacasallas@gmail.com>
2021-10-18 05:54:11 -0500

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,7 +15,7 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n / 2; i++)
 	{
 		aux = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = aux;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = aux;
 	}
 }
