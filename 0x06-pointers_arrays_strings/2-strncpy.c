c740dd951caa4d860b2644ca21193e353d6b493b
joacasallas <joacasallas@gmail.com>
2021-10-17 20:26:08 -0500

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,15 @@ int _strlen(char *s);
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, m;
+	int i;
 
-	m = _strlen(src) - n;
-	for (i = (_strlen(src) - 1); i < m; i++)
-	{
-		src[i] = 0;
-	}
-	for (i = 0; i < n && i < _strlen(dest); i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i++] = '\0';
 	}
 	return (dest);
 }
