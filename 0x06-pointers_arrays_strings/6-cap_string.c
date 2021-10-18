b56192136e86ef440bf566360e964c3c96a81f0b
joacasallas <joacasallas@gmail.com>
2021-10-18 08:03:05 -0500

    update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+int _strlen(char *src);
+
+/**
+ * cap_string -  capitalize all words of a string.
+ * @n: pointer to string to capitalize
+ * Return: pointer to string capitalized
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	for (i = 0; i < _strlen(n); i++)
+	{
+		if (n[i] == ' ')
+		{
+			if (n[i + 1] > 'a' && n[i + 1] < 'z')
+			{
+				n[i + 1] = n[i + 1] - 32;
+			}
+		}
+	}
+	return (n);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @src: string to check length
+ * Return: lenght of a string.
+ */
+
+int _strlen(char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
