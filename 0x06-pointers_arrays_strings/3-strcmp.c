12f4d395c215f65e690fab49f3e5a017e80ae104
joacasallas <joacasallas@gmail.com>
2021-10-18 02:13:57 -0500

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: First string to be compared
+ * @s2: Second string to be compared
+ * Return: <0: s1 is less than s2
+ * Return: >0: s1 is bigger than s2
+ * Return: =0: s1 equal to s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+	return (i - j);
+}
