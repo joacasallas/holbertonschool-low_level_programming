ccd270316fda73fafe55fb14a88b39033d3569c0
joacasallas <joacasallas@gmail.com>
2021-10-20 00:19:01 -0500

    update

---
+++ 0x06-pointers_arrays_strings/101-main2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(INT_MAX);
+	_putchar('\n');
+	return (0);
+	
+}
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,10 +8,11 @@
 
 void print_number(int n)
 {
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
 	}
 	if (n >= 0 && n <= 9)
 	{
