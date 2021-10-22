06f9eb9db11efd6e504c1b869150ee0ba47b5318
joacasallas <joacasallas@gmail.com>
2021-10-22 17:58:15 -0500

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,10 +17,15 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[i] == needle [j])
 			{
-				return (haystack + i);
+				j++;
 			}
+			else
+			{
+				break;
+			}
+			return (haystack + i);
 		}
 	}
 	return ('\0');
