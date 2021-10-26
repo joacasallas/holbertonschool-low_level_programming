d6071d0e2b84d921f4e3edf6363a0f9ef12dcc13
joacasallas <joacasallas@gmail.com>
2021-10-25 20:07:33 -0500

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,6 @@ int sqrt_check(int g, int c)
 	if (g * g > c)
 		return (-1);
 	return (sqrt_check(g + 1, c));
-	
 }
 
 /**
@@ -28,5 +27,4 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 		return (0);
 	return (sqrt_check(1, n));
-	
 }
