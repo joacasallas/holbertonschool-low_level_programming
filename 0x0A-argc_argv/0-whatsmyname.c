f80b5ed3d00a84b637157ab07d632c2fcde0fd30
joacasallas <joacasallas@gmail.com>
2021-10-27 12:57:28 -0500

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the program name
+ * @argc: count of the arguments
+ * @argv: array of pointers to the strings
+ * Return: 0 for success
+ */
+
+int main (int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	exit(EXIT_SUCCESS);
+}
