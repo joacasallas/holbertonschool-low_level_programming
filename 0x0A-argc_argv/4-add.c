3b6e1e180d546c0dc7283a3328244de0a10751b2
joacasallas <joacasallas@gmail.com>
2021-10-28 21:58:20 -0500

    update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - print the sum of positive numbers.
+ * If no number is passed to the program, print 0
+ * If one of the number contains symbols, print Error and return 1
+ * @argc: count of the arguments
+ * @argv: array of pointers to the strings
+ * Return: 0 for success otherwise 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
