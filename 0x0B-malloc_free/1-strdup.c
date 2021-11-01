ff9a6131cfa975c56238297a2c1c92968f89a91e
joacasallas <joacasallas@gmail.com>
2021-11-01 06:19:42 -0500

    update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - copy the string given as a parameter in a pointer.
+ * @str: pointer string source
+ * Return: pointer to the new string copy or NULL if str is null
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr_malloc;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	ptr_malloc = malloc(sizeof(char) * i);
+	if (ptr_malloc == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j <= i; j++)
+	{
+		ptr_malloc[j] = str[j];
+	}
+	ptr_malloc[j] = '\0';
+	return (ptr_malloc);
+}
