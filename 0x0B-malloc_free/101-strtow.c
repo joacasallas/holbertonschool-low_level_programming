17ca883e3488da8a27187f9af6db259ad62b652e
joacasallas <joacasallas@gmail.com>
2021-11-02 11:42:53 -0500

    update

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **strtow - split a string into words
+ * @str: pointer string source
+ * Return: pointer to an array of strings
+ * Each element of this array should contain a single word
+ * The last element of the returned array should be NULL
+ * Returns NULL if str == NULL or str == ""
+ * If your function fails, it should return NULL
+ */
+
+char **strtow(char *str)
+{
+	char **ptrStrings;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	ptrStrings = malloc(sizeof(char) * i + 1);
+	if (ptrStrings == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0, j = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+		{
+			i++;
+		}
+		else if (str[i] != ' ')
+		{
+			ptrStrings[i][j] = str[i];
+			if (str[i + 1] == ' ')
+			{
+				ptrStrings[i][j] = '\n';
+				ptrStrings[i][j] = '\0';
+				j++;
+			}
+		}
+	}
+	ptrStrings[i][j] = '\0';
+	return (ptrStrings);
+}
