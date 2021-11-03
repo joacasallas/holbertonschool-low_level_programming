35b6fec384969c49a7893a62f81a986acd05b103
joacasallas <joacasallas@gmail.com>
2021-11-03 11:59:30 -0500

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, mul;
+	long unsigned int num1, num2, mul;
 
 	if (argc != 3)
 	{
@@ -21,6 +21,6 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	mul = num1 * num2;
-	printf("%d\n", mul);
+	printf("%lu\n", mul);
 	return (0);
 }
