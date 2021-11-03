f9e8a23956968659c6977bc9540f0aecbb515aca
joacasallas <joacasallas@gmail.com>
2021-11-03 18:03:17 -0500

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_number(int n);
+
 /**
  * main -  multiplies two positive numbers and print de result.
  * @argc: count of arguments
@@ -19,12 +21,50 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+	if (mul > 4294967296)
+	{
+		print_number(mul);
+		return (0);
+	}
 	if (num1 < 0 || num2 < 0)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	mul = num1 * num2;
-	printf("%ld\n", mul);
+	else
+	{
+		printf("%ld\n", mul);
+	}
 	return (0);
+}
+
+/**
+ * print_number -  print an integer.
+ * @n: integer
+ * Return: Nothing.
+ */
+
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	for (i = 1000000000; i > 0; i /= 10)
+	{
+		if (n / i)
+		{
+			if ((n / i) % 10 < 0)
+				_putchar(-(n / i % 10) + '0');
+			else
+				_putchar((n / i % 10) + '0');
+		}
+		else if (n / i == 0 && i == 1)
+		{
+			_putchar(n / i % 10 + '0');
+		}
+	}
 }
