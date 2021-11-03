2633c5391b18eb4472a9ee4ba91e86cb633c5ce3
joacasallas <joacasallas@gmail.com>
2021-11-03 01:58:12 -0500

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,23 +4,28 @@
  * *_calloc - allocates memory for an array, using malloc.
  * The memory is set to zero
  * @nmemb: number of elements
- * @size: the size of the array
+ * @size: the size of each element
  * Return: pointer to the memory allocated
  * If nmemb or size is 0, then _calloc returns NULL
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr_malloc = 0;
+	char *ptr_malloc;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	ptr_malloc =malloc(nmemb * size);
+	ptr_malloc = malloc(nmemb * size);
 	if (ptr_malloc == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr_malloc[i] = 0;
 	}
 	return (ptr_malloc);
 }
