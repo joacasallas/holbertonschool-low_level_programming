a6262053c92a3f4c11614c2c82cdebdb6af7d2f8
joacasallas <joacasallas@gmail.com>
2021-11-03 08:55:38 -0500

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,7 +26,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		free(ptr);
-		ptr_realloc = malloc(new_size);
 		return (ptr);
 	}
 	if (new_size == 0)
