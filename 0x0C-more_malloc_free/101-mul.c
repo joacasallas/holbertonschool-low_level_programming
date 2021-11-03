5df20b50be27b1ba2174a4911a5a29bb186031ba
joacasallas <joacasallas@gmail.com>
2021-11-03 13:12:45 -0500

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,9 +17,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
+	if (num1 <= 0 || num2 <= 0)
+	{
+		printf("Error\n");
+	}
 	mul = num1 * num2;
 	printf("%ld\n", mul);
 	return (0);
