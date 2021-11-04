45e584ead2c47905eda550a93306651657d2b930
joacasallas <joacasallas@gmail.com>
2021-11-04 02:08:06 -0500

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _STRUCT_DOG_
+#define _STRUCT_DOG_
+
+/**
+ * struct dog - New struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: new structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
