94b6946607097eec45bce915fb34b7712a0fd967
joacasallas <joacasallas@gmail.com>
2021-11-03 23:05:10 -0500

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
 {
-	printf("%s\n",__BASE_FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
