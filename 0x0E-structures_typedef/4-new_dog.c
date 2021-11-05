2ec900a268b3403e034dcd09e3c09cb39ae27c2f
joacasallas <joacasallas@gmail.com>
2021-11-05 08:37:25 -0500

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,73 +1,80 @@
 #include "dog.h"
 
-char *_strdup(char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 /**
- * new_dog - creates a new dog.
- * @name: first type data struct
- * @age: second type data struct
- * @owner: third type data struct
- * Return: pointer to struct or NULL if the function fails
+ * new_dog - creates a new dog. Store a copy of name and owner
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: Pointer to new_dog or NULL if the function fails
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	new_dog *namedup;
-	new_dog *ownerdup;
 
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	if (name == NULL)
+	new_dog->name = malloc(sizeof(char) * _strlen(new_dog->name));
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
-		free(owner);
 		return (NULL);
 	}
-	if (owner == NULL)
+	new_dog->owner = malloc(sizeof(char) * _strlen(new_dog->owner));
+	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
-		free(name);
 		return (NULL);
 	}
-	new_dog->name = name;
+	new_dog->name = _strcpy(new_dog->name, name);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	new_dog->owner = owner;
-	new_dog->namedup = _strdup(name);
-	new_dog->ownerdup = _strdup(owner);
 	return (new_dog);
 }
 
+
 /**
- * _strdup - copy the string given as a parameter in a pointer.
- * @str: pointer string source
- * Return: pointer to the new string copy or NULL if str is null
+ * _strlen - returns the length of a string
+ * @s: string to check length
+ * Return: lenght of a string.
  */
 
-char *_strdup(char *str)
+int _strlen(char *s)
 {
-	int i, j;
-	char *ptr_malloc;
+	int i;
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
-	ptr_malloc = malloc(sizeof(char) * i + 1);
-	if (ptr_malloc == NULL)
+	return (i);
+}
+
+/**
+ * _strcpy -  copies the string pointed to by src, including the terminating \0
+ * to the buffer pointed to by dest.
+ * @dest: string result
+ * @src: string origin
+ * Return: string pointer dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
 	{
-		return (NULL);
+		dest[i] = src[i];
 	}
-	for (j = 0; j <= i; j++)
-	{
-		ptr_malloc[j] = str[j];
-	}
-	ptr_malloc[j] = '\0';
-	return (ptr_malloc);
+	return (dest);
 }
