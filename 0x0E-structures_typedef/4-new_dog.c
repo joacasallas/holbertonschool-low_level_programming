f062a9340140ead8e204ed3d630c5beffe35bc40
joacasallas <joacasallas@gmail.com>
2021-11-05 01:50:53 -0500

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
 
+char *_strdup(char *str);
+
 /**
  * new_dog - creates a new dog.
  * @name: first type data struct
@@ -11,6 +13,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	new_dog *namedup;
+	new_dog *ownerdup;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
@@ -32,5 +36,38 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
+	new_dog->namedup = _strdup(name);
+	new_dog->ownerdup = _strdup(owner);
 	return (new_dog);
+}
+
+/**
+ * _strdup - copy the string given as a parameter in a pointer.
+ * @str: pointer string source
+ * Return: pointer to the new string copy or NULL if str is null
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr_malloc;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	ptr_malloc = malloc(sizeof(char) * i + 1);
+	if (ptr_malloc == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j <= i; j++)
+	{
+		ptr_malloc[j] = str[j];
+	}
+	ptr_malloc[j] = '\0';
+	return (ptr_malloc);
 }
