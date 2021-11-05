134807b276580eefb724b349726a86b512bb0158
joacasallas <joacasallas@gmail.com>
2021-11-05 01:06:50 -0500

    update

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog.
+ * @name: first type data struct
+ * @age: second type data struct
+ * @owner: third type data struct
+ * Return: pointer to struct or NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
