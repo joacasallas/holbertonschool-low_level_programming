14ae4febae56c0b3964c7b871e3a78b994342a33
joacasallas <joacasallas@gmail.com>
2021-11-06 03:10:50 -0500

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL || size < 0 || action == NULL)
+	{
+		exit(98);
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
