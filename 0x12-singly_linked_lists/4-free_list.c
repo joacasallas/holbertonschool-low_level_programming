efec719d6f9e9d67431c6ea010adf37782ed5fba
joacasallas <joacasallas@gmail.com>
2021-11-15 01:31:57 -0500

    update

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * main - frees a list_t list.
+ * @head: pointer to list_t
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+		temp = head;
+	}
+}
