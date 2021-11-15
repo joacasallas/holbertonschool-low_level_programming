de9803e6e4916a76fd6e1b01a2e47c71d87671ba
joacasallas <joacasallas@gmail.com>
2021-11-15 13:11:16 -0500

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * first - prints You're beat! and yet, you must allow,\nI bore my house
+ * upon my back!\n before the main function is executed.
+ *
+ * Return: Nothing
+ */
+
+void first(void)
+{
+	char *str;
+
+	str = "and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("You're beat! %s", str);
+}
