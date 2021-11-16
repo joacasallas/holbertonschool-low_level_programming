9e02a7ea699435f497ac0bda20abe56543785f4c
joacasallas <joacasallas@gmail.com>
2021-11-16 10:17:30 -0500

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
+		while (*head != NULL)
+		{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
+		}
+		free(*head);
 	}
-	free(*head);
 }
