39c2d6387ae4fdd373ea19b362bacbd59faeb845
joacasallas <joacasallas@gmail.com>
2021-11-16 17:33:40 -0500

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,8 +15,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	while (i < index && head->next != NULL)
+	while (i < index)
 	{
+		if (head->next == NULL)
+		{
+			return (NULL);
+		}
 		head = head->next;
 		i++;
 	}
