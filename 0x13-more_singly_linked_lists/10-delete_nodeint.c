5948ac12b42058090c1c8c1555a1932b800c7ae0
joacasallas <joacasallas@gmail.com>
2021-11-19 15:54:13 -0500

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *temp = *head, *temp2 = *head;
 
-	if (*head == NULL)
+	if (!*head || !head)
 	{
 		return (-1);
 	}
@@ -33,7 +33,15 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-		*head = (*head)->next;
+		if ((*head)->next == NULL)
+		{
+			*head = NULL;
+			return (-1);
+		}
+		else
+		{
+			*head = (*head)->next;
+		}
 	}
 	return (1);
 }
