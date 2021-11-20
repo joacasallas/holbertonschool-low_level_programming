27dcd9028154ad7943e74180faa3bcb36326bdc5
joacasallas <joacasallas@gmail.com>
2021-11-19 20:10:59 -0500

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			temp2 = temp->next;
 			temp->next = temp2->next;
-			free(new);
+			free(temp2);
 			return (1);
 		}
 		temp = temp->next;
