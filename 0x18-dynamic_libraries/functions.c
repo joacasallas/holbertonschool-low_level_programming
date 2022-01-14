668d929d2e856fedcf772abcd6a376425d917edf
joacasallas <joacasallas@gmail.com>
2022-01-14 12:13:09 -0500

    update

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,60 @@
+#include "main2.h"
+
+/**
+ * add - sum 2 integers.
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the operation
+ */
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+/**
+ * sub - sub 2 integers.
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the operation
+ */
+
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * mul - mul 2 integers.
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the operation
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * div - div 2 integers.
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the operation
+ */
+
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * mod - mod 2 integers.
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the operation
+ */
+
+int mod(int a, int b)
+{
+    return (a % b);
+}
