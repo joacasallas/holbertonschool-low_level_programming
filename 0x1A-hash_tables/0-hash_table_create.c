b668452584678ee4822047c71af7b76e43da7ef3
joacasallas <joacasallas@gmail.com>
2022-02-02 17:43:43 -0500

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,15 +17,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	ht = malloc(sizeof(hash_table_t));
-	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht == NULL)
 	{
 		return (NULL);
 	}
 	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
-		ht->array = NULL;
+		ht->array[i] = NULL;
 	}
 	return (ht);
 }
