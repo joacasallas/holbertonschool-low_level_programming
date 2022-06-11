7038ae636970daed7e53e469c608456c6b2eef08
joacasallas <joacasallas@gmail.com>
2022-06-10 21:56:05 -0500

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -20,6 +20,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
